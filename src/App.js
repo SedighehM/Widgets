7f185afe425247de020cea5c6a07199f1ddeb084
unknown <semohammadkhani@gmail.com>
2022-07-20 22:23:10 -0400

    dropDown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 const items = [
   {
     title: "What is React?",
@@ -15,10 +16,29 @@ const items = [
     content: "You use React by creating components",
   },
 ];
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color of Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
